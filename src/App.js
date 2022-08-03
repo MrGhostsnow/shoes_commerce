f62719aeb49d7011f77ff8076064bda0fe7e0859
unknown <victorio5054@gmail.com>
2022-08-03 20:50:04 -0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
+import {Routes, Route} from 'react-router-dom'
 import Card from "./components/Card/Card";
 
 function App() {
   return (
     <div className="App">
       <img className="logo" src="./assets/images/logosnowshoes.png" alt="" />
-      <div className="Home">
-        <Card />
-      </div>
+      <Routes>
+        <div className="Home">
+          <Route
+          path="/"
+          element={<Card/>}/>
+        </div>
+      </Routes>
     </div>
   );
 }
